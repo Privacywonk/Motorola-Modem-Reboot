67b2f7ea4f97e816fa035424b5f181083787423a
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2020-06-08 22:56:30 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,18 +1,61 @@
 # Motorola-Modem-Reboot
-I wrote this so I could reboot my modem on a cron job without any extra hardware.
-It uses the JSON flavor of the defunct HNAP protocol to signal the modem to reboot.
-I've only tested it on the MB8600 model, let me know if it works on other models.
+This script provides metadata about a Motorola MB8600 modem which can be useful for integration other systems, such as home automation and monitoring.  It returns a JSON object in stdout.  Additionally, it can reboot the modem with the appropriate argument passed.
 
-Requirements:
+### Credit
+Based on https://github.com/aclytle/Motorola-Modem-Reboot.  Many thanks for the powerful tool you created.
+
+The original script automatically rebooted the modem, which may be dangerous in some scenarios.  A parameter was added to specifically request the reboot.  This implementation can also provide basic information about the modem if the reboot command is not provided.  Furthermore, the host, username, and password for the modem can all be specified from the command line if not using the default values.
+
+### Prerequisite Requirements
 * Python3
 * Requests
 
+To install `Requests` in Ubuntu Linux:
 ```
-usage: modem_reboot.py [-h] [--host HOST] [--password PASSWORD] [--dryrun]
+sudo apt-get install python-requests
+```
+
+To install `Requests` in MacOS:
+```
+python3 -m pip install requests
+```
+
+### Usage
+```
+usage: modem_reboot.py [-h] [--host HOST] [--username USERNAME]
+                       [--password PASSWORD] [--reboot]
 
 optional arguments:
   -h, --help           show this help message and exit
   --host HOST          Hostname or IP of your modem (Default: 192.168.100.1)
+  --username USERNAME  Admin username (Default: admin)
   --password PASSWORD  Admin password (Default: motorola)
-  --dryrun, -d         Logs in but doesn't reboot
+  --reboot             Reboots the modem
   ```
+
+### Example Request to Retrieve Data
+`~$ python3 modem_reboot.py --username myAdminUsername --password myPassword`
+
+### Example Pretty Print Response (Success)
+```
+{
+    "requestStatus": "SUCCESS",
+    "data": {
+        "softwareVersion": "8600-18.2.12",
+        "macAddress": "00:40:36:4E:B6:1F",
+        "serialNumber": "0408-MB8600-3252",
+        "operatorSoftwareVersion": "Prod_18.2_a4",
+        "wanStatus": "Connected",
+        "systemUptime": "0 days 06h:47m:26s",
+        "networkAccess": "Allowed"
+    }
+}
+```
+
+### Example Pretty Print Response (Failure)
+```
+{
+    "requestStatus": "ERROR",
+    "message": "Login failed."
+}
+```
--- modem_reboot.py
+++ modem_reboot.py
@@ -5,6 +5,7 @@ import argparse
 import requests
 import json
 
+
 class SurfboardHNAP:
 
     def __init__(self):
@@ -13,11 +14,13 @@ class SurfboardHNAP:
         self.cookie_id = None
         self.host = None
 
+
     def generate_keys(self, challenge, pubkey, password):
         privatekey = hmac.new(pubkey+password, challenge).hexdigest().upper()
         passkey = hmac.new(privatekey.encode(), challenge).hexdigest().upper()
         self.privatekey = privatekey
         return (privatekey, passkey)
+
 
     def generate_hnap_auth(self, operation):
         privkey = self.privatekey
@@ -27,52 +30,85 @@ class SurfboardHNAP:
         auth = hmac.new(privkey, auth_key.encode())
         return auth.hexdigest().upper() + ' ' + curtime
 
-    def _login_request(self, host):
+
+    def _login_request(self, host, username):
         url = 'http://{}/HNAP1/'.format(host)
-        headers = {#'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.34 Safari/537.36',
-                #'Content-Type' : 'application/json; charset=UTF-8',
-                #'Accept' : 'application/json, text/javascript, */*; q=0.01',
-                #'X-Requested-With' : 'XMLHttpRequest',
-                'SOAPAction' : '"http://purenetworks.com/HNAP1/Login"'}
-        payload = '{"Login":{"Action":"request","Username":"admin","LoginPassword":"","Captcha":"","PrivateLogin":"LoginPassword"}}'
+        headers = {'SOAPAction' : '"http://purenetworks.com/HNAP1/Login"'}
+        payload = '{"Login":{"Action":"request","Username":"' + username + '","LoginPassword":"","Captcha":"","PrivateLogin":"LoginPassword"}}'
 
-        r = self.s.post(url, headers=headers, data=payload, stream=True)
-        return r
+        try:
+                r = self.s.post(url, headers=headers, data=payload, stream=True)
 
-    def _login_real(self, host, cookie_id, privatekey, passkey):
+                return r
+        except:
+                returnValue = { "requestStatus":"ERROR", "message":"Login failed."}
+                print(json.dumps(returnValue))
+                exit(-1)
+
+
+    def _login_real(self, host, username, cookie_id, privatekey, passkey):
         url = 'http://{}/HNAP1/'.format(host)
         auth = self.generate_hnap_auth('Login')
-        headers = {'HNAP_AUTH' : auth,
-                #'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.34 Safari/537.36',
-                #'Content-Type' : 'application/json; charset=UTF-8',
-                #'Accept' : 'application/json, text/javascript, */*; q=0.01',
-                #'X-Requested-With' : 'XMLHttpRequest',
-                'SOAPAction' : '"http://purenetworks.com/HNAP1/Login"'}
+        headers = {'HNAP_AUTH' : auth, 'SOAPAction' : '"http://purenetworks.com/HNAP1/Login"'}
         cookies = {'uid' : '{}'.format(cookie_id),
                 'PrivateKey' : '{}'.format(privatekey)}
         payload = {'Login': {'Action': 'login',
                 'Captcha': '',
                 'LoginPassword': '{}'.format(passkey),
                 'PrivateLogin': 'LoginPassword',
-                'Username': 'admin'}}
+                'Username': '{}'.format(username)}}
 
-        r = self.s.post(url, headers=headers, cookies=cookies, json=payload)
-        return r
+        try:
+                r = self.s.post(url, headers=headers, cookies=cookies, json=payload)
 
-    def login(self, host, password):
+                return r
+        except:
+                returnValue = { "requestStatus":"ERROR", "message":"Login failed."}
+                print(json.dumps(returnValue))
+                exit(-1)
+
+
+    def login(self, host, username, password):
         self.host = host
-        r = self._login_request(host)
-        lrdata = json.loads(r.text)['LoginResponse']
-        cookie_id = lrdata['Cookie']
-        pubkey = lrdata['PublicKey']
-        challenge = lrdata['Challenge']
+        self.username = username
+        r = self._login_request(host, username)
+
+        #Validate there was a response from the server
+        try:
+                lrdata = json.loads(r.text)
+        
+        except:
+                returnValue = { "requestStatus":"ERROR", "message":"Unable to parse modem response."}
+                print(json.dumps(returnValue))
+                exit(-1)
+
+        if "LoginResponse" not in lrdata:
+                returnValue = { "requestStatus":"ERROR", "message":"LoginResponse not in modem response."}
+                print(json.dumps(returnValue))
+                exit(-1)
+
+        if "LoginResult" not in lrdata['LoginResponse']:
+                returnValue = { "requestStatus":"ERROR", "message":"LoginResult not in modem LoginResponse object."}
+                print(json.dumps(returnValue))
+                exit(-1)
+
+        #Validate the login response was successful
+        if lrdata['LoginResponse']['LoginResult'] != "OK":
+                returnValue = { "requestStatus":"ERROR", "message":"Login failed."}
+                print(json.dumps(returnValue))
+                exit(-1)
+
+        cookie_id = lrdata['LoginResponse']['Cookie']
+        pubkey = lrdata['LoginResponse']['PublicKey']
+        challenge = lrdata['LoginResponse']['Challenge']
 
         self.cookie_id = cookie_id
 
         privkey, passkey = self.generate_keys(challenge.encode(), 
                                               pubkey.encode(), 
                                               password.encode())
-        return self._login_real(host, cookie_id, privkey, passkey)
+        return self._login_real(host, username, cookie_id, privkey, passkey)
+
 
     def get_status(self):
         host = self.host
@@ -81,54 +117,37 @@ class SurfboardHNAP:
 
         url = 'http://{}/HNAP1/'.format(host)
         auth = self.generate_hnap_auth('GetMultipleHNAPs')
-        headers = {'HNAP_AUTH' : auth,
-                #'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.34 Safari/537.36',
-                #'Content-Type' : 'application/json; charset=UTF-8',
-                #'Accept' : 'application/json, text/javascript, */*; q=0.01',
-                'SOAPACTION' : '"http://purenetworks.com/HNAP1/GetMultipleHNAPs"',
-                #'Referer' : 'http://{}/MotoSecurity.html'.format(host)
-                }
-
-        '''payload = {'GetMultipleHNAPs': {'GetMotoStatusStartupSequence': '',
-                'GetMotoStatusConnectionInfo': '',
-                'GetMotoStatusDownstreamChannelInfo': '',
-                'GetMotoStatusUpstreamChannelInfo': '',
-                'GetMotoLagStatus': ''}}'''
+        headers = {'HNAP_AUTH' : auth, 'SOAPACTION' : '"http://purenetworks.com/HNAP1/GetMultipleHNAPs"'}
 
         cookies = {'uid' : '{}'.format(cookie_id),
                 'PrivateKey' : '{}'.format(privatekey)}
-        payload = {'GetMultipleHNAPs': {'GetMotoStatusSoftware': '',
-                'GetMotoStatusXXX': ''}}
 
-        r = self.s.post(url, headers=headers, cookies=cookies, json=payload)
-        return r
+        payload = {'GetMultipleHNAPs': {'GetMotoStatusSoftware': '', 'GetHomeConnection':'','GetMotoStatusConnectionInfo':''}}
 
-    def get_security(self):
-        host = self.host
-        cookie_id = self.cookie_id
-        privatekey = self.privatekey
+        try:
+                r = self.s.post(url, headers=headers, cookies=cookies, json=payload)
 
-        url = 'http://{}/HNAP1/'.format(host)
-        auth = self.generate_hnap_auth('GetMultipleHNAPs')
-        headers = {'HNAP_AUTH' : auth,
-                #'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.34 Safari/537.36',
-                #'Content-Type' : 'application/json',
-                #'Accept' : 'application/json',
-                'SOAPACTION' : '"http://purenetworks.com/HNAP1/GetMultipleHNAPs"',
-                #'Referer' : 'http://{}/MotoSecurity.html'.format(host),
-                #'Origin' : 'http://{}'.format(host),
-                #'Cookie' : 'uid={}; PrivateKey={}'.format(cookie_id, privatekey),
-                #'Accept-Encoding' : 'gzip, deflate',
-                #'Accept-Language' : 'en-US,en-XA;q=0.9,en;q=0.8'
-                }
+                jsonResponse = json.loads(r.text)
 
-        cookies = {'uid' : '{}'.format(cookie_id),
-                'PrivateKey' : '{}'.format(privatekey)}
-        payload = {'GetMultipleHNAPs': {'GetMotoStatusSecAccount': '',
-                'GetMotoStatusSecXXX': ''}}
+                #Parse the results to a simplified object
+                returnValue = { "requestStatus":"SUCCESS",
+                                "data":{
+                                "softwareVersion":jsonResponse['GetMultipleHNAPsResponse']['GetMotoStatusSoftwareResponse']['StatusSoftwareSfVer'],
+                                "macAddress":jsonResponse['GetMultipleHNAPsResponse']['GetMotoStatusSoftwareResponse']['StatusSoftwareMac'],
+                                "serialNumber":jsonResponse['GetMultipleHNAPsResponse']['GetMotoStatusSoftwareResponse']['StatusSoftwareSerialNum'],
+                                "operatorSoftwareVersion":jsonResponse['GetMultipleHNAPsResponse']['GetMotoStatusSoftwareResponse']['StatusSoftwareCustomerVer'],
+                                "wanStatus":jsonResponse['GetMultipleHNAPsResponse']['GetHomeConnectionResponse']['MotoHomeOnline'],
+                                "systemUptime":jsonResponse['GetMultipleHNAPsResponse']['GetMotoStatusConnectionInfoResponse']['MotoConnSystemUpTime'],
+                                "networkAccess":jsonResponse['GetMultipleHNAPsResponse']['GetMotoStatusConnectionInfoResponse']['MotoConnNetworkAccess'],
+                                }}
 
-        r = self.s.post(url, headers=headers, cookies=cookies, json=payload)
-        return r
+                print(json.dumps(returnValue))
+
+        except:
+                returnValue = { "requestStatus":"ERROR", "message":"Failed to retrieve status."}
+                print(json.dumps(returnValue))
+                exit(-1)
+
 
     def reboot(self):
         host = self.host
@@ -137,44 +156,42 @@ class SurfboardHNAP:
 
         url = 'http://{}/HNAP1/'.format(host)
         auth = self.generate_hnap_auth('SetStatusSecuritySettings')
-        headers = {'HNAP_AUTH' : auth,
-                #'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.34 Safari/537.36',
-                #'Content-Type' : 'application/json; charset=UTF-8',
-                #'Accept' : 'application/json, text/javascript, */*; q=0.01',
-                #'X-Requested-With' : 'XMLHttpRequest',
-                'SOAPAction' : '"http://purenetworks.com/HNAP1/SetStatusSecuritySettings"'}
+        headers = {'HNAP_AUTH' : auth, 'SOAPAction' : '"http://purenetworks.com/HNAP1/SetStatusSecuritySettings"'}
 
         cookies = {'uid' : '{}'.format(cookie_id),
                 'PrivateKey' : '{}'.format(privatekey)}
         payload = {'SetStatusSecuritySettings': {'MotoStatusSecurityAction': '1',
                 'MotoStatusSecXXX': 'XXX'}}
-        r = self.s.post(url, headers=headers, cookies=cookies, json=payload)
-        return r
+
+        try:
+                r = self.s.post(url, headers=headers, cookies=cookies, json=payload)
+                returnValue = {"requestStatus":"SUCCESS"}
+                print(json.dumps(returnValue))
+
+        except:
+                returnValue = { "requestStatus":"ERROR", "message":"Reboot failed."}
+                print(json.dumps(returnValue))
+                exit(-1)
 
 
 def get_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--host', default='192.168.100.1', 
-            help='Hostname or IP of your modem (Default: 192.168.100.1)')
-    parser.add_argument('--password', default='motorola', 
-            help='Admin password (Default: motorola)')
-    parser.add_argument('--dryrun', '-d', action='store_true', 
-                        help="Logs in but doesn't reboot")
-    return parser.parse_args()
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--host', default='192.168.100.1', help='Hostname or IP of your modem (Default: 192.168.100.1)')
+        parser.add_argument('--username', default='admin', help='Admin username (Default: admin)')
+        parser.add_argument('--password', default='motorola', help='Admin password (Default: motorola)')
+        parser.add_argument('--reboot', action='store_true', help="Reboots the modem")
+
+        return parser.parse_args()
+
 
 if __name__ == '__main__':
-    args = get_arguments()
-    host = args.host
-    password = args.password
+        args = get_arguments()
+        host = args.host
+        username = args.username
+        password = args.password
 
-    h = SurfboardHNAP()
-    r = h.login(host, password)
-    print('login: {}'.format(r))
-    r = h.get_status()
-    print('status: {}'.format(r))
-    if not args.dryrun:
-        r = h.reboot()
-        print('reboot: {}'.format(r))
-        print(r.text)
-    r = h.get_security()
-    print('get_security: {}'.format(r))
+        h = SurfboardHNAP()
+        h.login(host, username, password)
+        h.get_status()
+        if args.reboot:
+                r = h.reboot()
